3af92069765b360404e25874f916e8123bae4995
Kostas Stamatis <kstamatis@ekt.gr>
2022-09-20 13:05:40 +0300

    make country translations independent of the database id

--- core/components/asi/snippets/asi_search_results.php
+++ core/components/asi/snippets/asi_search_results.php
@@ -210,7 +210,9 @@ else {
     
     $translatedCountries = array();
     foreach($filters['countries'] as $country => $countryCount) {
-        $translated = $modx->lexicon('asi.'.$country);
+        $pos = strpos($country, ':');
+        $countryKey = substr($country, 0, $pos);
+        $translated = $modx->lexicon('asi.'.$countryKey);
         $translatedCountries[$country]['translated'] = $translated;
         $translatedCountries[$country]['count'] = $countryCount;
     }
