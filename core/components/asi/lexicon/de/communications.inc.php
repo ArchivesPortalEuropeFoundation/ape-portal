454e1100807f8333f8ba9903dfd687db0d0a8900
kstamatis <kstamatis@ekt.gr>
2022-10-06 18:50:29 +0000

    update 'asi' lexicon files

--- core/components/asi/lexicon/de/communications.inc.php
+++ core/components/asi/lexicon/de/communications.inc.php
@@ -1,6 +1,6 @@
 <?php
-$_lang['asi.confirm_receive_emails'] = 'Bitte kreuzen Sie, was Sie möchten E-Mails empfangen für';
+$_lang['asi.title_notification_preferences'] = 'Notification preferences';
+$_lang['asi.confirm_receive_emails'] = 'Please tick what you would like to receive emails for';
 $_lang['asi.label_newsletter'] = 'Newsletter';
-$_lang['asi.label_researcher_materials'] = 'Forscher Materialien';
-$_lang['asi.label_updates_to_saved_content'] = 'Updates zu meinen gespeicherten Inhalt';
-$_lang['asi.title_notification_preferences'] = 'Kein Behelf';
+$_lang['asi.label_researcher_materials'] = 'Researcher materials';
+$_lang['asi.label_updates_to_saved_content'] = 'Updates to my saved content';
--- core/components/asi/lexicon/de/language.inc.php
+++ core/components/asi/lexicon/de/language.inc.php
@@ -1,6 +1,5 @@
 <?php
 $_lang['asi.eng'] = 'Englisch';
 $_lang['asi.fra'] = 'Französisch';
-$_lang['asi.fra'] = 'Französisch';
-$_lang['asi.ger'] = 'Deutsche';
-$_lang['asi.spa'] = 'Sortieren / Filter';
+$_lang['asi.ger'] = 'Deutsch';
+$_lang['asi.spa'] = 'Spanisch';
